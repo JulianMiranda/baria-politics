e3923cd38de39673cfb8f9c3756a56222d99631b
JulianMiranda <jmirandauria@gmail.com>
2024-08-31 09:26:13 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import {Politics} from './Politics';
+
+function App() {
+	return (
+		<div className="App">
+			<header
+				style={{
+					backgroundColor: '#fff',
+					padding: 5,
+					marginTop: 50,
+					marginBottom: 50
+				}}
+				className="App-header"
+			>
+				<Politics />
+			</header>
+		</div>
+	);
+}
+
+export default App;
